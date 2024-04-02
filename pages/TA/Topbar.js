14b246bae6d6910774c4db6867966ce98c00b702
krish2592 <sachink9225s@gmail.com>
2024-04-02 14:20:57 +0530

    updated topbar

--- pages/TA/Topbar.js
+++ pages/TA/Topbar.js
@@ -9,9 +9,9 @@ export default function Topbar() {
   return (
     <Nav activeKey="/home" className={tb.topb} justify>
       <Nav.Item className={tb.nav}>
-      <Nav.Link href="TA/Dasboard">
+      {/* <Nav.Link href="TA/Dasboard">
         <Image src={iitbhilai} style={{ width: 50, marginRight: 150 }} />
-      </Nav.Link>
+      </Nav.Link> */}
         <Nav.Item className={tb.nav}>
           <Nav.Link href="/TA/course_feed" className={tb.navbuttons}>
             Course Feed
